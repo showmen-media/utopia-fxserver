08f9c86a1eb1fcc31ba3db0f1fa92cada7682b07
Philip Psychias <filippos@psychias.gr>
2025-06-10 10:45:07 +0000

    qbcore autodownload dependencies for qb-hud & fixes/comments

---
+++ autodownload/qb-spawn/server.lua
@@ -0,0 +1,15 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+--### Needs houses resource
+QBCore.Functions.CreateCallback('qb-spawn:server:getOwnedHouses', function(_, cb, cid)
+    if cid ~= nil then
+        local houses = MySQL.query.await('SELECT * FROM player_houses WHERE citizenid = ?', { cid })
+        if houses[1] ~= nil then
+            cb(houses)
+        else
+            cb({})
+        end
+    else
+        cb({})
+    end
+end)
