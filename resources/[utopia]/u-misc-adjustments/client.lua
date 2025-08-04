43e9e016f5339f463c44f533e5b438445c0fd099
Philip Psychias <filippos@psychias.gr>
2025-08-04 13:24:38 +0000

    Added u-lago-zancudo and moved stuff from misc-adjustments there

--- resources/[utopia]/u-misc-adjustments/client.lua
+++ resources/[utopia]/u-misc-adjustments/client.lua
@@ -7,9 +7,6 @@ Citizen.CreateThread(function()
 	end
 end)
 
--- Display Fort Zancudo on map
-SetMinimapComponent(15, true, 0)
-
 -- Disable vehicle weapons
 Citizen.CreateThread(function()
     while true do
@@ -25,28 +22,3 @@ Citizen.CreateThread(function()
     end
 end)
 
-
-local tmp_blips = {
-	{
-		title="Welcome Center", id=850, colour=0,
-		x = -1561.6, y = 2771.57, z = 17.31
-	},
-	{
-		title="Medbay", id=61, colour=0,
-		x = -1821.68, y = 3134.95, z = 32.81
-	}
-}
-
-Citizen.CreateThread(function()
-    for _, info in pairs(tmp_blips) do
-      info.blip = AddBlipForCoord(info.x, info.y, info.z)
-      SetBlipSprite(info.blip, info.id)
-      SetBlipDisplay(info.blip, 4)
-      SetBlipScale(info.blip, 1.0)
-      SetBlipColour(info.blip, info.colour)
-      SetBlipAsShortRange(info.blip, true)
-	  BeginTextCommandSetBlipName("STRING")
-      AddTextComponentString(info.title)
-      EndTextCommandSetBlipName(info.blip)
-    end
-end)
